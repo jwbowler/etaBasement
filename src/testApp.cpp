78167adfb6ed0dfb700b4d068e59ecc5cd7755a7
John Bowler <jwbowler@gmail.com>
2014-02-15 18:31:54 -0500

    added bass-mid-treble

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,4 +1,5 @@
 #include "testApp.h"
+#include <assert.h>
 #include <iostream>
 
 void testApp::setup(){
@@ -14,6 +15,13 @@ void testApp::setup(){
 
 void testApp::update(){
 	fft.update();
+
+	power = integrateFft(fft.getBins(), 0, fftBufferSize/2);
+	bass = integrateFft(fft.getBins(), bassMin, bassMax);
+	mid = integrateFft(fft.getBins(), midMin, midMax);
+	treble = integrateFft(fft.getBins(), trebleMin, trebleMax);
+
+	//std::cout << "POWER: " << power << " BASS: " << bass << ", MID: " << mid << ", TREBLE: " << treble << std::endl;
 }
 
 void testApp::draw(){
@@ -28,9 +36,12 @@ void testApp::draw(){
 
 	ofPushMatrix();
 	ofTranslate(16, 16*2 + 128);
-	ofDrawBitmapString("bass", 0, 0);
-	ofDrawBitmapString("mid", 64, 0);
-	ofDrawBitmapString("treble", 128, 0);
+	ofDrawBitmapString("bass", 0, 5);
+	ofDrawBitmapString("mid", 64, 5);
+	ofDrawBitmapString("treble", 128, 5);
+	ofRect(0, 20, 32, bass*100);
+	ofRect(64, 20, 32, mid*100);
+	ofRect(128, 20, 32, treble*100);
 	ofPopMatrix();
 
 	string msg = ofToString((int) ofGetFrameRate()) + " fps";
@@ -47,24 +58,20 @@ void testApp::plotFft(const vector<float>& buffer, const float scale) {
 	ofTranslate(0, scale);
 	ofScale(1, -scale);
 
-	int max = 0;
-	float maxVal;
-
 	ofBeginShape();
 	for (int i = 0; i < n; i++) {
-		if (buffer[i] > maxVal) {
-			max = i;
-			maxVal = buffer[i];
-		}
 		ofVertex(i, buffer[i] * 10);
-	}
-	float total;
-	for (int i = 4; i < 20; i++) {
-		total += buffer[i];
 	}
 	ofEndShape();
 
 	ofPopMatrix();
+}
 
-	std::cout << max << std::endl;
+float testApp::integrateFft(const vector<float>& bins, const unsigned int minFreq, const unsigned int maxFreq) {
+	assert(getBinFromFreq(maxFreq) < bins.size());
+	float total = 0;
+	for (int i = getBinFromFreq(minFreq); i < getBinFromFreq(maxFreq); i++) {
+		total += bins[i];
+	}
+	return total;
 }
